d89db27e2ef1aa5cc38fc817567bc6b7c0640ad1
Dyachkov Anton <dyachkov1984@inbox.ru>
2023-04-23 15:14:05 +0300

    Case_3

---
+++ Homework3/Task.java
@@ -0,0 +1,58 @@
+// 1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4,
+// при подаче массива другого размера необходимо бросить исключение MyArraySizeException.
+// 2. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и 
+//просуммировать. Если в каком-то элементе массива преобразование не удалось (например, 
+//в ячейке лежит символ или текст вместо числа), должно быть брошено исключение MyArrayDataException,
+// с детализацией в какой именно ячейке лежат неверные данные.
+// 3. В методе main() вызвать полученный метод, обработать возможные исключения
+// MyArraySizeException и MyArrayDataException и вывести результат расчета (сумму элементов, при условии что подали на вход корректный массив).
+
+
+
+
+package Homework3
+;
+
+public class ArraySum {
+
+    public static void main(String[] args) {
+        String[][] arr = {{"6", "7", "4", "4"}, {"5", "8", "7", "2"}, {"9", "6", "11", "52"}, {"3", "7", "15", "12"}};
+        try {
+            int sum = sumArray(arr);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+
+    public static int sumArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        if (arr.length != 4 || arr[0].length != 4) {
+            throw new MyArraySizeException("Неверный размер массива");
+        }
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные  в элементе с индексом  [" + i + "][" + j + "]");
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static class MyArraySizeException extends RuntimeException {
+        public MyArraySizeException(String message) {
+            super(message);
+        }
+    }
+
+    public static class MyArrayDataException extends RuntimeException {
+        public MyArrayDataException(String message) {
+            super(message);
+        }
+    }
+}
